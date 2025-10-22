01abd126c347b73b957ced3152783bf4299e79ef
kasskkk <kasjanlaboj@gmail.com>
2025-10-22 15:40:17 +0200

    predaisy

---
+++ client/src/App.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+
+function App() {
+
+  const [apartmens, setApartmens] = useState<Apartment[]>([]);
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/apartments')
+      .then(response => response.json())
+      .then(data => setApartmens(data))
+
+    return () => { }
+  }, [])
+
+  return (
+    <>
+      <h3 className="app" style={{ color: 'red' }}>RentA</h3>
+      <ul>
+        {apartmens.map((apartment) => (
+          <li key={apartment.id}>{apartment.name}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/lib/types/index.d.ts
@@ -0,0 +1,17 @@
+type Apartment = {
+    id: string
+    date: string
+    name: string
+    description: string
+    pricePerMonth: number
+    isAvailable: boolean
+    rooms: number
+    area: number
+    maxOccupants: number
+    city: string
+    street: string
+    buildingNumber: string
+    apartmentNumber: number | null
+    latitude: number
+    longitude: number
+}
