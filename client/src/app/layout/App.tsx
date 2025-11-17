51bb2e42076d0e8e2c7f35a0c7a6b0eddfefa461
kasskkk <kasjanlaboj@gmail.com>
2025-11-18 00:15:45 +0100

    sidebar position replace

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -17,19 +17,21 @@ function App() {
 
   return (
     <>
-      <div className="drawer lg:drawer-open">
-        <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content">
-          {/* Navbar */}
-          <NavBar />
-          {/* Page content here */}
-          <div className="p-4">
-            <ApartmentDashboard apartments={apartments} />
+      <div className="flex flex-col min-h-screen">
+        {/* Navbar */}
+        <NavBar />
+        {/* Drawer below navbar */}
+        <div className="drawer lg:drawer-open flex-1">
+          <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content">
+            {/* Page content here */}
+            <div className="p-4">
+              <ApartmentDashboard apartments={apartments} />
+            </div>
           </div>
-
+          {/* Sidebar */}
+          <SideBar />
         </div>
-        {/* Sidebar */}
-        <SideBar />
       </div>
     </>
   )
