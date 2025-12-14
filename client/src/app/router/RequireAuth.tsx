bf289f11905ee925f1e3ae345bf7a20371b3f7b7
kasskkk <kasjanlaboj@gmail.com>
2025-12-14 22:23:54 +0100

    add authorised routes

---
+++ client/src/app/router/RequireAuth.tsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from "react-router";
+import { useAccount } from "../../lib/hooks/useAccounts"
+
+export default function RequireAuth() {
+    const { currentUser, loadingUserIfno } = useAccount();
+
+    if (loadingUserIfno) return <span className="loading loading-spinner loading-xl"></span>
+
+    if (!currentUser) return <Navigate to="/login" replace />
+    return (
+        <Outlet />
+    )
+}
--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -8,22 +8,29 @@ import TestErrors from "../features/errors/TestErrors";
 import NotFound from "../features/errors/NotFound";
 import ServerError from "../features/errors/ServerError";
 import LoginForm from "../features/account/LoginForm";
+import RequireAuth from "./RequireAuth";
+import RegisterForm from "../features/account/RegisterForm";
 
 export const router = createBrowserRouter([
     {
-        path: '/',
-        element: <App />,
-        children: [
-            { path: '', element: <HomePage /> },
-            { path: 'apartments', element: <ApartmentDashboard /> },
-            { path: 'apartments/:id', element: <ApartmentDetail /> },
-            { path: 'createApartment', element: <ApartmentForm /> },
-            { path: 'editApartment/:id', element: <ApartmentForm /> },
-            { path: 'errors', element: <TestErrors /> },
-            { path: 'not-found', element: <NotFound /> },
-            { path: 'server-error', element: <ServerError /> },
-            { path: 'login', element: <LoginForm /> },
-            { path: '*', element: <Navigate replace to='not-found' /> },
-        ]
-    }
+        element: <RequireAuth />,
+        children: [{
+            path: '/',
+            element: <App />,
+            children: [
+                { path: '', element: <HomePage /> },
+                { path: 'apartments', element: <ApartmentDashboard /> },
+                { path: 'apartments/:id', element: <ApartmentDetail /> },
+                { path: 'createApartment', element: <ApartmentForm /> },
+                { path: 'editApartment/:id', element: <ApartmentForm /> },
+                { path: 'errors', element: <TestErrors /> },
+                { path: 'server-error', element: <ServerError /> },
+            ]
+        }]
+    },
+    { path: '/login', element: <LoginForm /> },
+    { path: '/register', element: <RegisterForm /> },
+    { path: '/not-found', element: <NotFound /> },
+
+    { path: '*', element: <Navigate replace to="/not-found" /> },
 ])
