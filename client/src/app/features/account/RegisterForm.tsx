375095da495d60a1a91e021475c463db5362dda4
kasskkk <kasjanlaboj@gmail.com>
2025-12-14 22:24:03 +0100

    add register form

--- client/src/app/features/account/RegisterForm.tsx
+++ client/src/app/features/account/RegisterForm.tsx
@@ -1,24 +1,44 @@
 import { useForm } from "react-hook-form";
 import { useAccount } from "../../../lib/hooks/useAccounts";
-import { loginSchema, type LoginSchema } from "../../../lib/schemas/loginSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { NavLink } from "react-router";
+import { registerSchema, type RegisterSchema } from "../../../lib/schemas/registerSchema";
 
-export default function LoginForm() {
-    const { loginUser } = useAccount();
+export default function RegisterForm() {
+    const { registerUser } = useAccount();
 
-    const { register, handleSubmit, formState: { errors, isValid, isSubmitting } } = useForm<LoginSchema>({
+    const { register, handleSubmit, setError, formState: { errors, isValid, isSubmitting } } = useForm<RegisterSchema>({
         mode: 'onTouched',
-        resolver: zodResolver(loginSchema)
+        resolver: zodResolver(registerSchema)
     });
 
-    const onSubmit = async (data: LoginSchema) => {
-        await loginUser.mutateAsync(data);
+    const onSubmit = async (data: RegisterSchema) => {
+        await registerUser.mutateAsync(data, {
+            onError: (error) => {
+                if (Array.isArray(error)) {
+                    error.forEach(err => {
+                        if (err.includes('Email')) setError('email', { message: err })
+                        else if (err.includes('Password')) setError('password', { message: err })
+                    })
+                }
+            }
+        });
     };
 
     return (
         <div className="flex justify-center items-center min-h-screen bg-base-100">
             <form onSubmit={handleSubmit(onSubmit)} className="bg-base-200 border-base-300 rounded-box w-xs border p-4">
+
+                <fieldset className="mb-4">
+                    <label className="label">Display name</label>
+                    <input
+                        {...register("displayName")}
+                        type="displayName"
+                        className={`input w-full ${errors.displayName ? 'input-error' : ''}`}
+                        placeholder="FirstName LastName"
+                    />
+                    {errors.displayName && <p className="text-error text-sm mt-1">{errors.displayName.message}</p>}
+                </fieldset>
 
                 <fieldset className="mb-4">
                     <label className="label">Email</label>
@@ -47,10 +67,10 @@ export default function LoginForm() {
                     type="submit"
                     disabled={!isValid || isSubmitting}
                 >
-                    Login
+                    Register
                 </button>
 
-                <NavLink to="/register" className="btn mt-2">Dont have account?</NavLink>
+                <NavLink to="/login" className="btn mt-2">Back to login</NavLink>
             </form>
         </div>
     );
--- client/src/lib/hooks/useAccounts.ts
+++ client/src/lib/hooks/useAccounts.ts
@@ -2,6 +2,8 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import type { LoginSchema } from "../schemas/loginSchema"
 import agent from "../api/agent"
 import { useNavigate } from "react-router";
+import type { RegisterSchema } from "../schemas/registerSchema";
+import toast from "react-hot-toast";
 
 export const useAccount = () => {
     const querClient = useQueryClient();
@@ -24,25 +26,39 @@ export const useAccount = () => {
             await agent.post('/account/logout')
         },
         onSuccess: () => {
-            querClient.removeQueries({queryKey: ['user']})
-            querClient.removeQueries({queryKey: ['apartments']})
+            querClient.removeQueries({ queryKey: ['user'] })
+            querClient.removeQueries({ queryKey: ['apartments'] })
             navigate('/login')
         },
         onError: (err) => console.log('logout error', err)
     })
 
-    const { data: currentUser } = useQuery({
+    const registerUser = useMutation({
+        mutationFn: async (creds: RegisterSchema) => {
+            await agent.post('/account/register', creds)
+        },
+        onSuccess: () => {
+            toast.success('Register successfull - lets log in');
+            navigate('/login');
+        }
+    })
+
+    const { data: currentUser, isLoading: loadingUserIfno } = useQuery({
         queryKey: ['user'],
         queryFn: async () => {
             const response = await agent.get<User>('/account/user-info');
             return response.data
         },
-        enabled: !querClient.getQueryData(['user'])
+        enabled: !querClient.getQueryData(['user']) 
+        && location.pathname !== '/login'
+        && location.pathname !== '/register'
     })
 
     return {
         loginUser,
         currentUser,
-        logoutUser
+        logoutUser,
+        loadingUserIfno,
+        registerUser
     }
 }
