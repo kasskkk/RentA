a92a0be8ca649148a201db1120794d2e64ae27b1
kasskkk <kasjanlaboj@gmail.com>
2026-01-01 18:17:09 +0100

    visual changes

--- client/src/app/features/apartments/dashboard/ApartmentCard.tsx
+++ client/src/app/features/apartments/dashboard/ApartmentCard.tsx
@@ -11,13 +11,15 @@ export default function ApartmentCard({ apartment }: Props) {
     const isOwner = currentUser?.userRole === "Owner";
 
     return (
-        <div className="card bg-base-50 w-auto shadow-sm">
+        <div className="card bg-base-50 w-auto shadow-lg">
             <figure className="m-0">
                 <svg xmlns="http://www.w3.org/2000/svg" width="125" height="250" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-house-icon lucide-house"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" /><path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" /></svg>
             </figure>
             <div className="card-body p-2">
                 <h2 className="card-title text-sm">{apartment.name}</h2>
                 <h2 className="card-title text-sm">{apartment.pricePerMonth} PLN Per month</h2>
+                <h2 className="card-title text-sm">{apartment.area} m&sup2;</h2>
+                <h2 className="card-title text-sm">Number of rooms: {apartment.rooms} </h2>
                 <p className="text-xs">{apartment.description}</p>
                 <div className="card-actions justify-end">
                     {isOwner && (
--- client/src/app/router/RequireAuth.tsx
+++ client/src/app/router/RequireAuth.tsx
@@ -4,7 +4,13 @@ import { useAccount } from "../../lib/hooks/useAccounts"
 export default function RequireAuth() {
     const { currentUser, loadingUserIfno } = useAccount();
 
-    if (loadingUserIfno) return <span className="loading loading-spinner loading-xl"></span>
+    if (loadingUserIfno) {
+        return (
+            <div className="flex h-screen w-screen items-center justify-center">
+                <span className="loading loading-spinner loading-lg"></span>
+            </div>
+        );
+    }
 
     if (!currentUser) return <Navigate to="/login" replace />
 
