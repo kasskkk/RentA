f3a089963e3fbfcd8dba6be635ea2d68aedb9c38
kasskkk <kasjanlaboj@gmail.com>
2026-01-02 11:50:52 +0100

    visual changes

--- client/src/app/features/apartments/dashboard/ApartmentCard.tsx
+++ client/src/app/features/apartments/dashboard/ApartmentCard.tsx
@@ -11,8 +11,8 @@ export default function ApartmentCard({ apartment }: Props) {
     const isOwner = currentUser?.userRole === "Owner";
 
     return (
-        <div className="card bg-base-50 w-auto shadow-lg">
-            <figure className="m-0">
+        <div className="card bg-base-100 w-auto shadow-lg">
+            <figure className="bg-base-200 m-0">
                 <svg xmlns="http://www.w3.org/2000/svg" width="125" height="250" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-house-icon lucide-house"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" /><path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" /></svg>
             </figure>
             <div className="card-body p-2">
--- client/src/app/features/profiles/ProfileAvatarCard.tsx
+++ client/src/app/features/profiles/ProfileAvatarCard.tsx
@@ -12,8 +12,20 @@ export default function ProfileAvatarCard({ profile }: Props) {
                 className="avatar tooltip tooltip-bottom"
                 data-tip={profile.displayName}
             >
-                <div className="w-24 h-24 rounded-full bg-neutral text-neutral-content flex items-center justify-center hover:shadow-lg transition">
-                    <span className="text-3xl">{profile.displayName?.[0]?.toUpperCase()}</span>
+                <div className="w-9 h-9 rounded-full">
+                    {profile?.imageUrl ? (
+                        <img
+                            src={profile.imageUrl}
+                            alt={profile.displayName?.[0]}
+                            className="w-full h-full object-cover"
+                        />
+                    ) : (
+                        <>
+                            <div className="w-full h-full flex items-center justify-center bg-gray-200 font-semibold">
+                                {profile?.displayName?.[0]?.toUpperCase() ?? "U"}
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </NavLink>
