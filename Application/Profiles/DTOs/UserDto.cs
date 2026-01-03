7f6ffddc9e0605345c5837ecf2cf621da099bb5c
kasskkk <kasjanlaboj@gmail.com>
2026-01-03 12:15:29 +0100

    phone number in registration

--- Application/Profiles/DTOs/UserDto.cs
+++ Application/Profiles/DTOs/UserDto.cs
@@ -7,6 +7,7 @@ public class UserDto
     public required string Id { get; set; }
     public required string Email { get; set; }
     public string? FirstName { get; set; }
+    public string? PhoneNumber { get; set; }
     public string? LastName { get; set; }
     public string? DisplayName { get; set; }
     public string? ImageUrl { get; set; }
